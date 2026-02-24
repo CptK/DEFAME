d7e829bde12535896ba16e237a3f45b908d0ecae
Marcus Kornmann <m-kornmann@gmx.de>
2026-02-24 09:49:36 +0000

    make config file argument

--- scripts/run_config.py
+++ scripts/run_config.py
@@ -1,5 +1,22 @@
 """Runs the experiment as configured by the specified configuration file."""
 
+
+import argparse
+
+from defame.eval.utils import run_experiment
+
+
 if __name__ == '__main__':
-    from defame.eval.utils import run_experiment
-    run_experiment(config_file_path="in/defame.yaml")
+    parser = argparse.ArgumentParser(
+        description="Run an experiment based on the provided configuration file."
+    )
+    parser.add_argument(
+        "config_file_path",
+        type=str,
+        default="/DEFAME/config/experiments/averitec.yaml",
+        help="Path to the configuration file for the experiment."
+    )
+    args = parser.parse_args()
+
+    print(f"Running experiment with config file: {args.config_file_path}")
+    run_experiment(config_file_path=args.config_file_path)
